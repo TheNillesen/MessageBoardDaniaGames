7ed645b606549f67568f9cd4d02e2d9cb67bf94e
theni <theni@LAPTOP-Q9ORS9IL>
2017-04-19 11:17:24 +0200

    pis

--- MessageBoard/MessageBoard/Form1.Designer.cs
+++ MessageBoard/MessageBoard/Form1.Designer.cs
@@ -34,8 +34,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(575, 700);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
