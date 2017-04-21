b8e89b33ac5e59d3cf8a17ed76c537765b717e8e
Nvhen <Nvhen@LAPTOP-VUTMMO7N>
2017-04-21 11:00:29 +0200

    Roger roger now with databasssee drop the baaaseeee

---
+++ MessageBoard/MessageBoard/Database.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace MessageBoard
+{
+    sealed partial class Database
+    {
+        SQLiteConnection dbConn = new
+        SQLiteConnection("Data Source=data.db;Version=3;");
+
+
+        public void DataBridge()
+        {
+            if (!File.Exists("data.db"))
+                CreateDatabase();
+            else
+                dbConn.Open();
+        }
+
+
+        private void CreateDatabase()
+        {
+            //Opretter en ny database
+            SQLiteConnection.CreateFile("data.db");
+            
+          
+            dbConn.Open();
+
+            string Mandag = "create table Mandag (id integer primary key, Lokale, Skibsnavn, Aktivitet, Lærer)";
+            string Tirsdag = "create table Tirsdag (id integer primary key, Lokale, Skibsnavn, Aktivitet, Lærer)";
+            string Onsdag = "create table Onsdag (id integer primary key, Lokale, Skibsnavn, Aktivitet, Lærer)";
+            string Torsdag = "create table Torsdag (id integer primary key, Lokale, Skibsnavn, Aktivitet, Lærer)";
+            string Fredag = "create table Fredag (id integer primary key, Lokale, Skibsnavn, Aktivitet, Lærer)";
+
+            SQLiteCommand mandag = new SQLiteCommand(Mandag, dbConn);
+            SQLiteCommand tirsdag = new SQLiteCommand(Tirsdag, dbConn);
+            SQLiteCommand onsdag = new SQLiteCommand(Onsdag, dbConn);
+            SQLiteCommand torsdag = new SQLiteCommand(Torsdag, dbConn);
+            SQLiteCommand fredag = new SQLiteCommand(Fredag, dbConn);
+
+            mandag.ExecuteNonQuery();
+            tirsdag.ExecuteNonQuery();
+            onsdag.ExecuteNonQuery();
+            torsdag.ExecuteNonQuery();
+            fredag.ExecuteNonQuery();
+
+
+
+        }
+
+
+
+    }
+}
--- MessageBoard/MessageBoard/Program.cs
+++ MessageBoard/MessageBoard/Program.cs
@@ -17,6 +17,10 @@ namespace MessageBoard
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            Database db = new Database();      
+            db.DataBridge();
+
         }
     }
 }
