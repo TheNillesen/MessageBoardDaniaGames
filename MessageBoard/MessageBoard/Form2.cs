12d1b78e0db87c6a33ad11d92ed59c96590ab1fa
theni <theni@LAPTOP-Q9ORS9IL>
2017-04-21 11:45:42 +0200

    123456

--- MessageBoard/MessageBoard/Form2.cs
+++ MessageBoard/MessageBoard/Form2.cs
@@ -13,6 +13,8 @@ namespace MessageBoard
 {
     public partial class Form2 : Form
     {
+
+        //fields for mouse drag
         private const int WM_NCHITTEST = 0x84;
         private const int HTCLIENT = 0x1;
         private const int HTCAPTION = 0x2;
@@ -65,9 +67,134 @@ namespace MessageBoard
                     if ((int)m.Result == HTCLIENT)
                         m.Result = (IntPtr)HTCAPTION;
                     return;
-                    break;
+                    
             }
             base.WndProc(ref m);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //mandag
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //tirsdag
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //onsdag
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //torsdag
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //fredag
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //mandag
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //tirsdag
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //onsdag
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //torsdag
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //fredag
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            //mandag
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            //tirsdag
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            //onsdag
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            //torsdag
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            //fredag
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            //mandag
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            //tirsdag
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            //onsdag    
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            //torsdag
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            //fredag
+        }
+
+        private void button41_Click(object sender, EventArgs e)
+        {
+            //mandag
+        }
+
+        private void button40_Click(object sender, EventArgs e)
+        {
+            //tirsdag
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+            //onsdag
+        }
+
+        private void button39_Click(object sender, EventArgs e)
+        {
+            //torsdag
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+            //fredag
         }
     }
 }
